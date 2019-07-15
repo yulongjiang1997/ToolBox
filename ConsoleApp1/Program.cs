ae36121d414f1fa6b11530f8d986e084386b7d9a
aidenxl <aidenxl@126.com>
2019-07-15 16:57:35 +0800

    6666

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
